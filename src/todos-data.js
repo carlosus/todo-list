d068b53e8885967a5ed8131c57d6bc97ecc950d2
Carlos Us Santiago <cdus12@gmail.com>
2019-05-20 17:40:18 -0700

    added todos data

---
+++ src/todos-data.js
@@ -0,0 +1,24 @@
+const todos = [
+    {
+        task: 'Buy Tortilla Chips',
+        completed: true
+    },
+    {
+        task: 'Buy Avocados',
+        completed: false
+    },
+    {
+        task: 'Buy Red Onions',
+        completed: false
+    },
+    {
+        task: 'Buy Tomatoes',
+        completed: false
+    },
+    {
+        task: 'Buy Jalapeños',
+        completed: false
+    } 
+];
+
+export default todos;
